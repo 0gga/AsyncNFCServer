bb4fa1d975f74806eb19cf975ce1f3b115f9a122
Ogga <oscar@friisweb.dk>
2025-10-10 13:53:02 +0200

    fix

--- Reader/servers/CLI_TCP.c
+++ Reader/servers/CLI_TCP.c
@@ -9,67 +9,75 @@
 #define BUFSIZE_RX 200
 #define BUFSIZE_TX 256
 
-void error(const char *msg)
-{
-	perror(msg);
-	exit(1);
+void error(const char* msg) {
+    perror(msg);
+    exit(1);
 }
 
-int main(int argc, char *argv[])
-{
-	printf("Starting server...\n");
+int main(int argc, char* argv[]) {
+    printf("Starting server...\n");
 
-	int sockfd, newsockfd, portno;
+    int sockfd, newsockfd, portno;
 
-	socklen_t clilen;
-	uint8_t bufferRx[BUFSIZE_RX];
-	uint8_t bufferTx[BUFSIZE_TX];
-	struct sockaddr_in serv_addr, cli_addr;
-	int n;
+    socklen_t clilen;
+    uint8_t bufferRx[BUFSIZE_RX];
+    uint8_t bufferTx[BUFSIZE_TX];
+    int n;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
-		error("ERROR opening socket");
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        return 1;
+    }
 
-	printf("Binding...\n");
-	bzero((char *)&serv_addr, sizeof(serv_addr));
-	portno = 3000;
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
-	if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-		error("ERROR on binding");
+    ////////////////////////////////////////
 
-	printf("Listen...\n");
-	listen(sockfd, 5);
+    constexpr char ip[]     = "172.16.15.2";
+    constexpr uint16_t port = 9000;
 
-	clilen = sizeof(cli_addr);
+    ////////////////////////////////////////
 
-	for (;;)
-	{
-		printf("Accept...\n");
-		newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port   = htons(port);
+    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
+        perror("sendto");
+        close(sockfd);
+        return 1;
+    }
 
-		if (newsockfd < 0)
-			error("ERROR on accept");
-		else
-			printf("Accepted\n");
+    if (bind(sockfd, reinterpret_cast<sockaddr*>(&serv_addr), sizeof(serv_addr)) < 0) {
+        perror("bind");
+        close(sockfd);
+        return 1;
+    }
 
-		bzero(bufferRx, sizeof(bufferRx));
-		n = read(newsockfd, bufferRx, sizeof(bufferRx));
+    // Waiting for clients - Size of queue.
+    listen(sockfd, 5);
 
-		if (n < 0)
-			error("ERROR reading from socket");
-		printf("Message: %s\n", (char *)bufferRx);
+    printf("Listening...\n");
+    clilen = sizeof(cli_addr);
 
-		snprintf((char *)bufferTx, sizeof(bufferTx), "Got message: %s", (char *)bufferRx);
+    while (true) {
+        printf("Accepting...\n");
+        newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
 
-		n = write(newsockfd, bufferTx, strlen((char *)bufferTx));
-		if (n < 0)
-			error("ERROR writing to socket");
+        if (newsockfd < 0) error("ERROR on accept");
+        else printf("Accepted\n");
 
-		close(newsockfd);
-	}
-	close(sockfd);
-	return 0;
+        readTextTCP(newsockfd, bufferRx, sizeof(bufferRx));
+        n = read(newsockfd, bufferRx, sizeof(bufferRx));
+
+        if (n < 0) error("ERROR reading from socket");
+        printf("Message: %s\n", (char*)bufferRx);
+
+        snprintf((char*)bufferTx, sizeof(bufferTx), "Got message: %s", (char*)bufferRx);
+
+        n = write(newsockfd, bufferTx, strlen((char*)bufferTx));
+        if (n < 0) error("ERROR writing to socket");
+
+        close(newsockfd);
+    }
+    close(sockfd);
+    return 0;
 }
